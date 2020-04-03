8d8b35f3d41845f1918a92dc1440615563f6f0ac
Jessica Wang <wangjessica02@gmail.com>
2020-04-03 14:52:43 -0400

    got link functionality wroking

--- app/controllers/ootd_recommendation_controller.rb
+++ app/controllers/ootd_recommendation_controller.rb
@@ -1,2 +1,7 @@
 class OotdRecommendationController < ApplicationController
+
+    # When you get to the page it'll recommend you an OOTD
+    def recommend
+        puts "YEP!!!!"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/ootd", to: "ootd_recommendation#recommend", as: "recommend"
 
   # maps requests to the root of the application to the welcome controller's index action
   root 'welcome#index'
